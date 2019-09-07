214877261adca51b61c05581b2cc6cd80b08cf51
Tsemach Mizrachi <tsemach@music-1.Home>
2019-09-07 06:18:40 +0300

    remove

--- intel/middlewre.ts
+++
@@ -1,5 +0,0 @@
-import * as express from 'express';
-
-export default interface Middleware {
-  add(express: express.Application): express.Handler;
-}
--- intel/routeinterfce.ts
+++
@@ -1,5 +0,0 @@
-import * as express from 'express';
-
-export default interface Route {
-  add(express: any): express.Router;
-}
